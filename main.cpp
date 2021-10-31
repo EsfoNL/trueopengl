8934de4903dd09d3d56a8bbb32906f691b7c562e
EsfoNL <verzamelmeel@gmail.com>
2021-10-31 08:46:55 +0100

    REMOVED GLUT

--- main.cpp
+++ main.cpp
@@ -1,15 +1,9 @@
 #include <GL/glew.h>
-#include <Gl/freeglut.h>
 #include <iostream>
+#include <windows.h>
 
-void display() {
-    glutSwapBuffers();
-}
+void display();
 
 int main(int argc, char** argv) {
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DOUBLE|GLUT_RGBA);
-    int win = glutCreateWindow("hello");
-    glutDisplayFunc(display);
-    glutMainLoop();
+    
 }
