1059f1075e372df703e9f8db5f25cf7372f60637
EsfoNL <verzamelmeel@gmail.com>
2021-10-31 23:20:15 +0100

    window works now

--- main.cpp
+++ main.cpp
@@ -1,22 +1,53 @@
-#include <GL/glew.h>
-#include <iostream>
+#ifndef UNICODE
+#define UNICODE
+#endif
+
 #include <windows.h>
-#include <string>
 
 LRESULT mymessageHandler(HWND hwnd, UINT uint, WPARAM wparam, LPARAM lparam) {
-    return LRESULT(0);
+    switch (uint) {
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        return 0;
+    }
+    return DefWindowProc(hwnd, uint, wparam, lparam);
 }
 
-int main(int argc, char** argv) {
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR pCmdLine, int nCmdShow) {
     LPCWSTR windowname = L"HELLO";
     windowname = L"miauw";
-    HMODULE instance = GetModuleHandle(NULL);
-    
     WNDCLASSEXW windclass = {};
-    windclass.hInstance = instance;
+    windclass.cbSize = sizeof(windclass);
+    windclass.hInstance = hInstance;
     windclass.lpfnWndProc = &mymessageHandler;
     windclass.lpszClassName = windowname;
+
     RegisterClassExW(&windclass);
-    HWND window = CreateWindowExW(0, windowname, windowname, WS_OVERLAPPEDWINDOW, 0, 0, 100, 100, NULL, NULL, instance, NULL);
-    ShowWindow(window, SW_NORMAL);
+
+    HWND window = CreateWindowExW(
+        WS_EX_APPWINDOW,
+        windowname,
+        windowname,
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        NULL,
+        NULL,
+        hInstance,
+        NULL
+    );
+    
+    if (window == NULL) {
+        return 404;
+    }
+    ShowWindow(window, nCmdShow);
+    MSG msg = { };
+    while (GetMessage(&msg, NULL, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+    return 0;
 }
