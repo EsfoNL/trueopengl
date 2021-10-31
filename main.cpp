e097ba9c48a867130c116ab7b83b76031931834b
EsfoNL <verzamelmeel@gmail.com>
2021-10-31 11:51:09 +0100

    created window structure and initialization

--- main.cpp
+++ main.cpp
@@ -1,10 +1,22 @@
 #include <GL/glew.h>
 #include <iostream>
 #include <windows.h>
+#include <string>
 
-void display();
+LRESULT mymessageHandler(HWND hwnd, UINT uint, WPARAM wparam, LPARAM lparam) {
+    return LRESULT(0);
+}
 
 int main(int argc, char** argv) {
-    HMODULE hello = GetModuleHandle(NULL);
-    std::cout << hello;
+    LPCWSTR windowname = L"HELLO";
+    windowname = L"miauw";
+    HMODULE instance = GetModuleHandle(NULL);
+    
+    WNDCLASSEXW windclass = {};
+    windclass.hInstance = instance;
+    windclass.lpfnWndProc = &mymessageHandler;
+    windclass.lpszClassName = windowname;
+    RegisterClassExW(&windclass);
+    HWND window = CreateWindowExW(0, windowname, windowname, WS_OVERLAPPEDWINDOW, 0, 0, 100, 100, NULL, NULL, instance, NULL);
+    ShowWindow(window, SW_NORMAL);
 }
