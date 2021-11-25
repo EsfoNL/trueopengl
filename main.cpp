ba706977139b623c7be542a39ecf3daafab07b7e
EsfoNL <verzamelmeel@gmail.com>
2021-11-25 21:03:48 +0100

    setup opengl

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #endif
 
 #include <windows.h>
-#include <Gl\gl.h>
+#include <GL\glew.h>
 #include <chrono>
 #include <string>
 #include <array>
@@ -26,6 +26,7 @@ void switchcursor(HCURSOR &cursor, HWND &hwnd);
  * @return int 
  */
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR pCmdLine, int nCmdShow) {
+    
     //variables
     std::thread nothing;
     HBRUSH backgroundbrush = CreateSolidBrush(RGB(0,0,0));
@@ -72,40 +73,62 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR pCmdLine,
     ShowWindow(window, nCmdShow);
     MSG msg = { };
 
+    //! setup ipengl context
     HDC hdc = GetDC(window);
+    PIXELFORMATDESCRIPTOR pixelformat = {
+        sizeof(pixelformat),                                        //size of pixelformat
+        1,                                                          //version number
+        PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER, //support OpenGL + window drawing + double buffered
+        PFD_TYPE_RGBA,                                              //pixel color type: rgba
+        24,                                                         //color 24 bit depth (r + g + b)
+        0, 0, 0, 0, 0, 0,                                           //unused color bits
+        8,                                                          //alpha channel 8 bit depth
+        0,                                                          //shift bit ignored
+        0,                                                          //no accumilation buffer
+        0, 0, 0, 0,                                                 //ignored accum bits
+        32,                                                         //z-buffer 32 bit depth
+        0,                                                          //no stencil buffer
+        0,                                                          //no auxiliary buffer
+        PFD_MAIN_PLANE,                                             //main layer
+        0,                                                          //reserved
+        0, 0, 0                                                     //layer masks ignored
+    };
+    int iPixelformat = ChoosePixelFormat(hdc, &pixelformat);
+    SetPixelFormat(hdc, iPixelformat, &pixelformat);
+    HGLRC hglrc = wglCreateContext(hdc);
+    wglMakeCurrent(hdc, hglrc);
+    GLenum err = glewInit();
+
+    
+    
+    //! first render
+    static const GLfloat buffer[] = {
+        -1.0f, -1.0f,  0.0f,
+         1.0f, -1.0f,  0.0f,
+         1.0f,  1.0f,  0.0f,
+    };
+    
+    GLuint bufferid;
+    
+    glGenBuffers(1, &bufferid);
+    glBindBuffer(GL_ARRAY_BUFFER, bufferid);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(buffer), buffer, GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, bufferid);
+    glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,0,(void*)0);
+    glDrawArrays(GL_TRIANGLES, 0,3);
+    glDisableVertexAttribArray(0);
+
+    SwapBuffers(hdc);
+
 
     while (GetMessage(&msg, NULL, 0, 0)) {
         TranslateMessage(&msg);
         
         //! message handeling area
         switch (msg.message) {
-            //toggles between arrow pointer and cross pointer
-            //also manages rgb state
-            case WM_LBUTTONUP:
-            case WM_RBUTTONUP:
-                switchcursor(cursor, window);
-            case WM_KEYUP:
-                {   
-                    switch (msg.wParam) {
-                        //triggers on r and toggles rgb on
-                        case 0x52:
-                            rgb = !rgb;
-                            if (rgb) {
-                                std::thread(dorgb, std::ref(rgb), std::ref(globalclock), std::ref(window), std::ref(hz), std::ref(backgroundbrush), std::ref(hdc)).detach();
-                            }
-                            break;
-                        //triggers on i, increases hz
-                        case 0x49:
-                            hz *= 2;
-                            break;
-                        //triggers on o, increases hz
-                        case 0x4F:
-                            hz /= 2;
-                            break;
-                    }
-                }
-            case WM_PAINT:
-                paintwindow(backgroundbrush, hdc, window);
+
         }
 
         DispatchMessage(&msg);
@@ -132,54 +155,4 @@ LRESULT CALLBACK mymessageHandler(HWND hwnd, UINT uint, WPARAM wparam, LPARAM lp
         return 0;
     }
     return DefWindowProc(hwnd, uint, wparam, lparam);
-}
-
-/**
- * @brief makes the background of the window go rgb if reference rgb param is true;
- * 
- * @param rgb refrence to a bool used for turning rgb on and off
- * @param globalclock 
- * @param hwnd 
- * @param hz 
- * @param backgroundbrush 
- */
-int dorgb(bool &rgb, std::chrono::steady_clock &globalclock, HWND &hwnd, float &hz, HBRUSH &backgroundbrush, HDC &hdc) {
-    std::chrono::time_point untilwaittime = globalclock.now() + std::chrono::milliseconds((int)(1000/hz));
-    std::uniform_int_distribution randgen(0,255);
-    std::random_device randsource;
-    COLORREF color;
-
-    while (rgb) {
-        color = RGB(randgen(randsource),randgen(randsource),randgen(randsource));
-        backgroundbrush = CreateSolidBrush(color);
-        paintwindow(backgroundbrush, hdc, hwnd);
-        if (globalclock.now() > untilwaittime) {
-            untilwaittime = globalclock.now();
-        } else {
-            std::this_thread::sleep_until(untilwaittime);
-        }
-        untilwaittime += std::chrono::milliseconds((int)(1000/hz));
-    }
-
-    return 0;
-}
-
-/**
- * @brief toggles between arrow and cross pointer, if not one of these it switches to cross
- * 
- */
-void switchcursor(HCURSOR &cursor, HWND &hwnd) {
-    //checks if the current cursor is the cross, if not sets it to cross, otherwise sets it to pointer
-    cursor == (HCURSOR)LoadCursor(NULL, IDC_CROSS) ? cursor = (HCURSOR)LoadCursor(NULL, IDC_ARROW) : cursor = (HCURSOR)LoadCursor(NULL, IDC_CROSS);
-    SetClassLongPtr(hwnd, -12, (LONG_PTR)cursor);
-    SetCursor(cursor);
-}
-
-void paintwindow(HBRUSH brush, HDC &hdc, HWND &hwnd) {
-    RECT rc;
-    GetClientRect(hwnd, &rc);
-    SetMapMode(hdc, MM_ANISOTROPIC); 
-    SetWindowExtEx(hdc, 100, 100, NULL); 
-    SetViewportExtEx(hdc, rc.right, rc.bottom, NULL); 
-    FillRect(hdc, &rc, brush);
 }
